8495931e8c982345fed99e3cf613a894a342fe26
Salauddin Nagori <salanagori333@gmail.com>
2025-06-22 20:07:36 +0530

    added more functions in our matrix header file

--- src/operations/mlfw_operations_double.c
+++ src/operations/mlfw_operations_double.c
@@ -5,16 +5,67 @@
 #include<mlfw_operations.h>
 mlfw_column_vec_double * mlfw_multiply_double_row_vector_with_column_vector(mlfw_row_vec_double *left_vector, mlfw_column_vec_double *right_vector)
 {
-return NULL;
+	mlfw_column_vec_double *column_vector;
+	dimension_t left_vector_size;
+	dimension_t right_vector_size;
+	double value=0;
+	if(left_vector==NULL || right_vector==NULL) return NULL;
+	left_vector_size=mlfw_row_vec_double_get_size(left_vector);
+	right_vector_size=mlfw_column_vec_double_get_size(right_vector);
+	if(left_vector_size!=right_vector_size) return NULL;
+	column_vector=mlfw_column_vec_double_create_new(1);
+	if(column_vector==NULL) return NULL;
+
+	for(index_t i=0;i<left_vector_size;i++)
+	{
+		value+=(mlfw_row_vec_double_get(left_vector,i)*mlfw_column_vec_double_get(right_vector,i));
+	}
+	mlfw_column_vec_double_set(column_vector, 0, value);
+	return column_vector;
 }
 
 mlfw_column_vec_double * mlfw_multiply_double_matrix_with_column_vector(mlfw_mat_double *left_matrix, mlfw_column_vec_double *right_vector)
 {
-return NULL;
+	mlfw_column_vec_double *column_vector;
+	dimension_t left_matrix_rows, left_matrix_columns;
+	dimension_t right_vector_size;
+	double value, left_value, right_value;
+	if(left_matrix==NULL || right_vector==NULL) return NULL;
+	mlfw_mat_double_get_dimensions(left_matrix, &left_matrix_rows, &left_matrix_columns);
+	right_vector_size=mlfw_column_vec_double_get_size(right_vector);
+	if(left_matrix_columns!=right_vector_size) return NULL;
+	column_vector=mlfw_column_vec_double_create_new(left_matrix_rows);
+	if(column_vector==NULL) return NULL;
+	for(index_t i=0;i<left_matrix_rows;i++)
+	{
+		value=0;
+		for(index_t j=0;j<left_matrix_columns;j++)
+		{
+			left_value=mlfw_mat_double_get(left_matrix, i, j);
+			right_value=mlfw_column_vec_double_get(right_vector,j);
+			value+=(left_value*right_value);
+		}
+		mlfw_column_vec_double_set(column_vector, i, value);
+	}
+
+	return column_vector;
 }
 
 mlfw_column_vec_double * mlfw_subtract_double_column_vector(mlfw_column_vec_double *left_vector, mlfw_column_vec_double *right_vector)
 {
-return NULL;
+	mlfw_column_vec_double *column_vector;
+	dimension_t left_vector_size,right_vector_size;
+	if(left_vector==NULL || right_vector==NULL) return NULL;
+	left_vector_size=mlfw_column_vec_double_get_size(left_vector);
+	right_vector_size=mlfw_column_vec_double_get_size(right_vector);
+	if(left_vector_size!=right_vector_size) return NULL;
+	column_vector=mlfw_column_vec_double_create_new(left_vector_size);
+	if(column_vector==NULL) return NULL;
+	for(index_t i=0;i<left_vector_size;i++)
+	{
+		double value=mlfw_column_vec_double_get(left_vector, i)-mlfw_column_vec_double_get(right_vector, i);
+		mlfw_column_vec_double_set(column_vector, i, value);		
+	}
+	return column_vector;
 }
 
